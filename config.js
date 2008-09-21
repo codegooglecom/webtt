e8b0f1d0a57772e59538d7507fc2c710996fbd2f
Tim.today <Tim.today@38c5e998-87be-11dd-a5c9-f1483cedc3eb>
2008-09-21 09:33:37 +0000

    webtt 1.0.alpha

---
+++ config.js
@@ -0,0 +1,68 @@
+
+/**
+ * @author Tim
+ */
+/**
+ * support languages
+ *
+ *"ar">阿拉伯文
+ *"bg">保加利亚文
+ *"pl">波兰语
+ *"ko">朝鲜语
+ *"da">丹麦语
+ *"de">德语
+ *"ru">俄语
+ *"fr">法语
+ *"fi">芬兰语
+ *"nl">荷兰语
+ *"cs">捷克语
+ *"hr">克罗地亚文
+ *"ro">罗马尼亚语
+ *"no">挪威语
+ *"pt">葡萄牙语
+ *"ja">日语
+ *"sv">瑞典语
+ *"es">西班牙语
+ *"el">希腊语
+ *"it">意大利语
+ *"hi">印度文
+ *"en">英语
+ *"tw">中文(繁体)
+ *"cn">中文(简体)
+ *
+ */
+var translate_to = Array('en', 'ja', 'ko', 'ar', 'ru', 'cn', 'tw');
+
+
+
+
+
+/**
+ * tags and method need to translate
+ */
+var translate_tags = {
+    "a": "innerHTML",
+    "b": "innerHTML",
+    "td": "innerHTML",
+    "div": "innerHTML",
+    "span": "innerHTML",
+    "p": "innerHTML",
+    "li": "innerHTML",
+    "th": "innerHTML",
+    "h1": "innerHTML",
+    "h2": "innerHTML",
+    "h3": "innerHTML",
+    "h4": "innerHTML",
+    "h5": "innerHTML",
+    "h6": "innerHTML",
+    "title": "innerHTML",
+    "label": "innerHTML",
+    "input": "value",
+    "button": "value",
+    "img": "title",
+    "img": "alt",
+    "area": "title"
+};
+
+
+
---
+++ label.js
@@ -0,0 +1,29 @@
+/**
+ * @author Tim
+ */
+var lang_label = {
+    "cn": "简体中文",
+    "tw": "繁體中文",
+    "en": "english",
+    "ja": "日本語",
+    "ko": "한국어", //朝鲜
+    "fr": "Français", //法国
+    "es": "Español", //西班牙
+    "de": "Deutsch", //德国
+    "ru": "Российская", //俄国
+    "ar": "العربية", //阿拉伯文
+    "bg": "Български", //保加利亚文
+    "pl": "Polski", //波兰语
+    "da": "Dansk", //丹麦语
+    "fi": "Suomi", //芬兰语
+    "nl": "Nederlands", //荷兰语
+    "cs": "Česky", //捷克语
+    "hr": "Hrvatski", //克罗地亚文
+    "ro": "Română", //罗马尼亚语
+    "no": "Norsk", //挪威语
+    "sv": "Svenska", //瑞典语
+    "el": "Ελληνικά", //希腊语
+    "it": "Italiano", //意大利语
+    "hi": "हिन्दी", //印度文
+};
+
